6c6539e9e4187d17e14a8461d7582d14c323b7c4
qh2231 <43402879+HaileyHao@users.noreply.github.com>
2021-11-10 11:42:39 -0500

    update

--- Codes/Pick Central Variable/NAsummary.R
+++ Codes/Pick Central Variable/NAsummary.R
@@ -8,9 +8,13 @@ all_SeriesCode <- unique(nodisagg$SeriesCode) # indo:253 # gua:280
 all_measure <- unique(nodisagg$UniqueID) # indo:266 # gua:297
 
 
-possible_NA_measure <- nodisagg %>% filter(is.na(Value)) %>% select(UniqueID) # indo 54 # gua:62
+
+# Measure:
+
+# ineligible part 1: measure with all NAs: 
+# indo 12 # gua:13
 NA_measure <- nodisagg %>% 
-  filter(UniqueID %in% possible_NA_measure$UniqueID) %>% 
+  # filter(UniqueID %in% possible_NA_measure$UniqueID) %>% 
   group_by(UniqueID) %>% 
   mutate(n = n()) %>% 
   ungroup() %>% 
@@ -18,74 +22,73 @@ NA_measure <- nodisagg %>%
   group_by(UniqueID) %>% 
   mutate(n_na = n()) %>% 
   mutate(all_na = ifelse(n_na == n, 1, 0)) %>% 
-  filter(all_na == 1) %>% 
-  select(UniqueID) %>% 
+  filter(all_na == 1) %>%
+  select(UniqueID) %>%
   unique()
-# measure with all NAs: indo 12 # gua:13
-
-isna_measure <- nodisagg %>% 
-  filter(UniqueID %in% possible_NA_measure$UniqueID) %>% 
-  group_by(UniqueID) %>% 
-  mutate(n = n()) %>% 
-  ungroup() %>% 
-  filter(is.na(Value)) %>% 
-  group_by(UniqueID) %>% 
-  mutate(n_na = n()) %>% 
-  mutate(all_na = ifelse(n_na == n, 1, 0)) 
-possible_NA_indicator <- isna_measure %>% 
-  filter(all_na == 1) %>% 
-  ungroup() %>% 
-  select(Indicator) %>% 
-  unique()
-NA_indicator <- nodisagg %>% 
-  select(UniqueID, Indicator) %>% 
-  filter(Indicator %in% possible_NA_indicator$Indicator) %>% 
-  group_by(Indicator) %>% 
-  mutate(n = n()) %>% 
-  ungroup() %>% 
-  mutate(NA_measure = ifelse(UniqueID %in% NA_measure$UniqueID, 1, 0)) %>% 
-  unique() %>% 
-  filter(NA_measure == 1) %>% 
-  group_by(Indicator) %>% 
-  mutate(n_na = n()) %>% 
-  mutate(all_na = ifelse(n == n_na, 1, 0)) %>% 
-  filter(all_na == 1) %>% 
-  select(Indicator) %>% 
-  unique()
-# indicator with all measures with all NAs: indo:3 # gua:4
 
 
-# measures with only one record other than NAs (can't do correlation): indo 63 # gua:77
+# ineligible part 2: measures with only one record other than NAs (can't do correlation): 
+# indo 63 # gua:77
 one_measure <- nodisagg %>% 
   filter(!is.na(Value)) %>%
   group_by(UniqueID) %>% 
   mutate(n = n()) %>% 
   ungroup() %>% 
-  filter(n == 1) 
-
-# indicator with all measures with only one record (can't do correlation): indo:31 # gua:35
-isna_indicator <- nodisagg %>% 
-  filter(Indicator %in% one_measure$Indicator) %>% 
-  group_by(Indicator) %>% 
-  mutate(n = n()) %>% 
-  ungroup() %>%
-  filter(UniqueID %in% one_measure$UniqueID) %>% 
-  group_by(Indicator) %>% 
-  mutate(n_one = n()) %>% 
-  mutate(all_one = ifelse(n == n_one, 1, 0))
-one_indicator <- isna_indicator %>% 
-  select(Indicator) %>% 
+  filter(n == 1) %>% 
+  select(UniqueID) %>% 
   unique()
 
 
 # ineligible measures: indo:75 # gua:90
-ineligible_measure <- rbind(NA_measure, one_measure)
+ineligible_measure <- rbind(NA_measure, one_measure) %>% unique()
 
 
-# ineligible indicators: indo:34 # gua:39
-ineligible_indicator <- rbind(NA_indicator, one_indicator)
 
-# eligible indicators: indo:89 # gua:84
+
+
+# Indicator:
+
+# ineligible part 1: indicator with all measures with all NAs: 
+# indo:6 # gua:6
+NA_indicator <- nodisagg %>% 
+  select(UniqueID, Indicator) %>% 
+  unique() %>% 
+  group_by(Indicator) %>% 
+  mutate(n = n()) %>% # number of measures under each indicator
+  ungroup() %>% 
+  mutate(NA_measure = ifelse(UniqueID %in% NA_measure$UniqueID, 1, 0)) %>% 
+  unique() %>% 
+  filter(NA_measure == 1) %>% 
+  group_by(Indicator) %>% 
+  mutate(n_na = n()) %>% # number of measures with no data under each indicator
+  mutate(all_na = ifelse(n == n_na, 1, 0)) %>% 
+  filter(all_na == 1) %>%
+  select(Indicator) %>%
+  unique()
+
+
+
+# indicator with all ineligible measures: 
+# indo:29 # gua:31
+ineligible_indicator <- nodisagg %>% 
+  select(UniqueID, Indicator) %>% 
+  unique() %>% 
+  group_by(Indicator) %>% 
+  mutate(n = n()) %>% # number of measures under each indicator
+  ungroup() %>% 
+  mutate(one_measure = ifelse(UniqueID %in% one_measure$UniqueID, 1, 0)) %>% # measure with only 1 obs other than NAs
+  unique() %>% 
+  mutate(NA_measure = ifelse(UniqueID %in% NA_measure$UniqueID, 1, 0)) %>% # measure with all NAs
+  filter(one_measure == 1 | NA_measure == 1) %>% 
+  group_by(Indicator) %>% 
+  mutate(n_ineligible = n()) %>% # number of ineligible measures
+  mutate(all_one = ifelse(n == n_ineligible, 1, 0)) %>% 
+  filter(all_one == 1) %>%
+  select(Indicator) %>%
+  unique()
+
+
+# eligible indicators: indo:94 # gua:92
 eligible_indicator <- nodisagg %>% 
   select(Indicator) %>% 
   unique() %>% 
@@ -93,22 +96,26 @@ eligible_indicator <- nodisagg %>%
   filter(eligible == 1) %>% 
   select(Indicator)
 
-# eligible indicators with multiple measures: indo:34 # gua:34
-eligible_indicator_multiple <- nodisagg %>% select(UniqueID, Indicator) %>% 
+# eligible indicators with multiple measures: indo:40 # gua:42
+eligible_indicator_multiple <- nodisagg %>% 
+  select(UniqueID, Indicator) %>% 
   unique() %>% 
   filter(Indicator %in% eligible_indicator$Indicator) %>% 
   group_by(Indicator) %>% 
   mutate(n = n()) %>% 
-  filter(n > 1) %>% 
-  select(Indicator) %>% 
+  filter(n > 1) %>%
+  select(Indicator) %>%
   unique()
 
 
 write.csv(eligible_indicator, 
-          "/Users/hailey/Documents/GitHub/G5055_Practicum_Project2/Data/variable types/gua_eligible_indicator_all.csv", 
+          "/Users/hailey/Documents/GitHub/G5055_Practicum_Project2/Data/variable_types/gua_eligible_indicator_all.csv", 
           row.names = FALSE)
 
 write.csv(eligible_indicator_multiple, 
-          "/Users/hailey/Documents/GitHub/G5055_Practicum_Project2/Data/variable types/gua_eligible_indicator_multimeasure.csv", 
+          "/Users/hailey/Documents/GitHub/G5055_Practicum_Project2/Data/variable_types/gua_eligible_indicator_multimeasure.csv", 
           row.names = FALSE)
+
+
+
 
