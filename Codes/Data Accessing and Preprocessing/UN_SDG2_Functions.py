6df5a6835ecc0b008842a39be188ab86283d8268
connixu <y.connie.xu@gmail.com>
2021-12-08 09:41:00 -0500

    Updated Data and Preprocessing Script

--- Codes/Data Accessing and Preprocessing/UN_SDG2_Functions.py
+++ Codes/Data Accessing and Preprocessing/UN_SDG2_Functions.py
@@ -1,6 +1,50 @@
 import pandas as pd 
 import numpy as np 
+import requests
+import json
+from pprint import pprint 
+import itertools, functools, operator
+import country_converter as coco
 
+def obtain_indicator_data(country): 
+    geo_area_code = coco.convert(names=str(country), to='UNcode')
+    
+    headers={
+    'Content-Type': 'application/x-www-form-urlencoded',
+    'Accept': 'application/octet-stream',
+    }
+    
+    data=[
+      ('goal', '1'),
+      ('goal', '2'),
+      ('goal', '3'),
+      ('goal', '4'),
+      ('goal', '5'),
+      ('goal', '6'),
+      ('goal', '7'),
+      ('goal', '8'),
+      ('goal', '9'),
+      ('goal', '10'),
+      ('goal', '11'),
+      ('goal', '12'),
+      ('goal', '13'),
+      ('goal', '14'),
+      ('goal', '15'),
+      ('goal', '16'),
+      ('goal', '17'),
+      ('areaCodes', str(geo_area_code)),
+      ('timePeriodStart', '2012'),
+      ('timePeriodEnd', '2022'),
+    ]
+    
+    tocsv=requests.post('https://unstats.un.org/SDGAPI/v1/sdg/Goal/DataCSV', headers=headers, data=data)
+    country_content=tocsv.content
+    csv_name = country + ".csv"
+
+    csv_file = open(csv_name, 'wb')
+    csv_file.write(country_content)
+    csv_file.close()
+    
 # This function is designed to generate a unique ID and transform data into a year format. 
 def preprocess(df): 
     df_copy  = df.copy()
